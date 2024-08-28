f0b2dafcf0d73977a68119eb397640fdc08d924c
Dev. Pablo H. <devpabloh@gmail.com>
2024-08-28 16:03:33 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,13 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    devtool: "source-map",
+    entry: "./src/index.js",
+    module: {
+        rules: [
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {loader: ""}
+        ]
+    }
+}

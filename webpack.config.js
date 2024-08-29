096177371d4b801d66be24ba6100a0b18a39dcf6
Dev. Pablo H. <devpabloh@gmail.com>
2024-08-29 11:28:03 -0300

    att

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         extensions: [".js", ".jsx"]
     },
     plugin: [
-        new HtmlWebPackPlugin({
+        new SomeWebpackPlugin({
             template: "./public/index.html"
         }),
     ],
